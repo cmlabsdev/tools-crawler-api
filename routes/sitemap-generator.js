c14694564b4def8e1af7482eeb8ec48a38d382a1
WisnuDs <182410103009@cs.unej.ac.id>
2021-03-15 13:09:20 +0700

    add endpoint download and delete for sitemap

--- routes/sitemap-generator.js
+++ routes/sitemap-generator.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-router.get('/download/:folder', (req, res) => {
+router.get('/sitemap-generator/download/:folder', (req, res) => {
   const folder = req.params.folder.replace('..', '').replace('/', '')
-  if (fs.existsSync(`${process.cwd()}/resource/sitemap/${folder}/sitemap.xml`)) {
-    res.download(`${process.cwd()}/resource/sitemap/${folder}/sitemap.xml`)
+  if (fs.existsSync(`${process.cwd()}/storage/sitemap/${folder}/sitemap.xml`)) {
+    res.download(`${process.cwd()}/storage/sitemap/${folder}/sitemap.xml`)
   } else {
     res.status(404).send({
       code: 404,
@@ -13,5 +13,29 @@ router.get('/download/:folder', (req, res) => {
     })
   }
 });
+
+router.delete('/sitemap-generator/delete/:folder', (req, res)=>{
+  let folder = req.params.folder.replace('..','').replace('/','')
+  if(fs.existsSync(`${process.cwd()}/storage/sitemap/${folder}/sitemap.xml`)){
+    fs.unlink(`${process.cwd()}/storage/sitemap/${folder}/sitemap.xml`, err => {
+      if (err){
+        res.status(500).send({
+          code : 500,
+          message : err.message
+        })
+      }else {
+        res.status(200).send({
+          code : 200,
+          message : "File was deleted"
+        })
+      }
+    })
+  }else{
+    res.status(404).send({
+      code : 404,
+      message : "file not found"
+    })
+  }
+})
 
 module.exports = router;
